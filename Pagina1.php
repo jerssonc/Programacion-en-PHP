c0d3982df817e22322b7207080dba4362cad1581
jsoganep <contact@gmail.com>
2021-02-10 17:50:51 -0500

    Pagina1 - img

---
+++ Pagina1.php
@@ -0,0 +1,42 @@
+<html>
+<head>
+	<title>Pagina1</title>
+</head>
+<body bgcolor='yellow' text='blue'>
+	<font color= '#00ff00' > Jersson </font> Chacon 
+
+<!-- Listas -->
+	<ol>  <!-- Listas ordenadas -->
+		<li>Jersson</li>
+ <!-- Listas con números se modifica con type = 'A' cambiandolo a letras, 'I' a números romanos o en un número especifico con start='#' en la etiqueta ol-->
+		<li>Chacon</li>
+	</ol>
+	<ol type='A'>  <!-- Listas ordenadas -->
+		<li>Jersson</li> <!-- Listas con números -->
+		<li>Chacon</li>
+	</ol>
+	<ol type='I'>  <!-- Listas ordenadas -->
+		<li>Jersson</li> <!-- Listas con números -->
+		<li>Chacon</li>
+	</ol>
+
+<!-- Listas desordenadas -->
+	<ul>  <!-- Listas ordenadas -->
+		<li>Jersson</li>
+ <!-- se modifican con type = 'circle' 'square' en la etiqueta ol-->
+		<li>Chacon</li>
+	</ul>
+	<ul type='circle'>  <!-- Listas ordenadas -->
+		<li>Jersson</li> <!-- Listas con números -->
+		<li>Chacon</li>
+	</ul>
+	<ul type='square'>  <!-- Listas ordenadas -->
+		<li>Jersson</li> <!-- Listas con números -->
+		<li>Chacon</li>
+	</ul>
+
+<!-- imagenes -->
+<img src ='img/AllenW.jpg'>
+
+</body>
+</html>
